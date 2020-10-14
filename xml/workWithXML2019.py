d9da1e5a97f3dec99c888d7f8d5f056010b73b40
Пользователь <C:\Users\User\AppData\Roaming\The Bat!>
2020-10-14 15:22:05 +0300

    'add'

--- xml/workWithXML2019.py
+++ xml/workWithXML2019.py
@@ -4,7 +4,7 @@ from xml.dom import minidom
 from openpyxl import load_workbook, workbook
 #from openpyxl import workbook
 
-wb = load_workbook(filename="./excel/08_10-2020-6.xlsx", data_only=True)
+wb = load_workbook(filename="./excel/44-10_2019!.xlsx", data_only=True)
 ws = wb.active
 
 names = ws.iter_rows(min_row=1, min_col=1, max_row=ws.max_row, max_col=1, values_only=True)
