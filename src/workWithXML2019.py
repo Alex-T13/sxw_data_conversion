54289a8fa032aa2cd1a394c4cad8fe5da774dd6a
Alexander-PC <morphix313@gmail.com>
2021-03-05 23:54:33 +0300

    second

--- src/workWithXML2019.py
+++ src/workWithXML2019.py
@@ -1,10 +1,10 @@
-#import xml.dom.minidom
+#import src.dom.minidom
 
 from xml.dom import minidom
 from openpyxl import load_workbook, workbook
 #from openpyxl import workbook
 
-wb = load_workbook(filename="./excel/10.8-7-9-2020Cap.xlsx", data_only=True)
+wb = load_workbook(filename="./excel/44-10_2019!.xlsx", data_only=True)
 ws = wb.active
 
 names = ws.iter_rows(min_row=1, min_col=1, max_row=ws.max_row, max_col=1, values_only=True)
@@ -13,12 +13,26 @@ summs = ws.iter_rows(min_row=1, min_col=3, max_row=ws.max_row, max_col=3, values
 wb.close()
 
 th_names = [item for sublist in names for item in sublist]
-new_names = list(map(str, th_names))
 th_quants = [item for sublist in quants for item in sublist]
-new_quants = list (map ( float, th_quants))
 th_summs = [item for sublist in summs for item in sublist]
-new_summs = list(map(float, th_summs))
 
+new1_names = []
+new1_quants = []
+new1_summs = []
+
+i=3
+while i < len(th_names):
+    new1_names.append(th_names[i])
+    new1_quants.append(th_quants[i])
+    new1_summs.append(th_summs[i])
+    i += 4
+
+#th_names = [item for sublist in names for item in sublist]
+new_names = list(map(str, new1_names))
+#th_quants = [item for sublist in quants for item in sublist]
+new_quants = list(map(float, new1_quants))
+#th_summs = [item for sublist in summs for item in sublist]
+new_summs = list(map(float, new1_summs))
 
 # создаём объект
 doc = minidom.Document()
@@ -106,8 +120,8 @@ while i < len(new_names):
     # stoimost
     stoimost = doc.createElement('stoimost')
     stoimost.setAttribute('mat', str(new_summs[i]))
-    stoimost.setAttribute('tr', str(round(tr*new_quants[i], 2)))
-    stoimost.setAttribute('pryam', str(round(tr*new_quants[i]+new_summs[i], 2)))
+    stoimost.setAttribute('tr', str(round(tr * new_quants[i], 2)))
+    stoimost.setAttribute('pryam', str(round(tr * new_quants[i] + new_summs[i], 2)))
     stoimost.setAttribute('ohr', '0')
     stoimost.setAttribute('pp', '0')
     rascenka.appendChild(stoimost)
@@ -125,7 +139,7 @@ while i < len(new_names):
     resurs.setAttribute('norma', '1')  # klv
     resurs.setAttribute('klv', str(new_quants[i]))
     resurs.setAttribute('cena_0', str(mat))
-    resurs.setAttribute('stm_0', str(round(mat*new_quants[i], 2)))
+    resurs.setAttribute('stm_0', str(round(mat * new_quants[i], 2)))
     resurs.setAttribute('tr', str(tr))
     resurs.setAttribute('tr_rub', '0')
     resurs.setAttribute('cena_tr', '9,6')
@@ -136,6 +150,20 @@ while i < len(new_names):
     i += 1
 
 xml_str = doc.toprettyxml(indent="  ")
-with open("product.xml", "w") as f:
+with open("product.src", "w") as f:
     f.write(xml_str)
 
+
+""" <rascenka npp="1" obosn="СЦЕН-МТ-001" naim="Сайдинг Extra color могано 3660мм s-0.85m2/шт" idGrw="7" klv="15" ed_izm="ШТ" vid_ohr_pp="12" uniq_lab_ohr_pp="5.1" tip="101" cena_0="3.29">
+      <nabor_kf>
+       <koef naim="1-й коэффициент к расценке (справочно)" mat="1" tr="1"/>
+       <koef naim="2-й коэффициент к расценке (справочно)" mat="1" tr="1"/>
+       <koef naim="3-й коэффициент к расценке (справочно)" mat="1" tr="1"/>
+       <koef_total_ohr_pp naim="Коэффициент по группе работ (5.1)" mat="1" tr="1" ohr="1" pp="1"/>
+      </nabor_kf>
+      <cena mat="3" tr="0.29" pryam="3.29" prc_ohr="71.02" prc_pp="47.58"/>
+      <stoimost mat="45" tr="4.35" pryam="49.35" ohr="0" pp="0"/>
+      <materialy>
+       <resurs obosn="СЦЕН-МТ-001" kodcic="" naim="Сайдинг Extra color могано 3660мм s-0.85m2/шт" ed_izm="ШТ" norma="1" klv="15" cena_0="3" stm_0="45" tr="0.29" tr_rub="0" cena_tr="9.6"/>
+      </materialy>
+     </rascenka> """
